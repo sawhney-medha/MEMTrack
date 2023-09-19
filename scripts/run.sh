1f85ad9690eebe6f006863c325902afe123bcc43
sawhney-medha <sawhney.medha@gmail.com>
2023-09-19 16:38:54 -0400

    updating run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,18 +3,12 @@
 set -e 
 
 #RUN ONLY INFERENCE WITH EVAL(GT DATA)
-#bash Scripts/detection/inference_eval.sh 
+bash scripts/inference.sh 
 #run with plot and plt gt
-bash Scripts/tracking/tracking.sh 
-bash Scripts/analysis/test_set_eval.sh
+bash scripts/tracking.sh 
+bash scripts/test_set_eval.sh
 
-# #RUN ONLY INFERENCE NO EVAL(NO GT DATA)
-# bash Scripts/detection/inference_no_eval.sh
+# RUN ONLY INFERENCE NO EVAL(NO GT DATA)
+ bash scripts/inference_no_eval.sh
 # #run only with plot
-# bash Scripts/tracking/tracking.sh 
-
-
-# #ONLY INFERENCE SINGLE CATEGORY LABELS
-# bash Scripts/samples/inference_label_singlecategory.sh
-# bash Scripts/tracking/tracking.sh
-# bash Scripts/analysis/test_set_eval.sh
+# bash scripts/tracking.sh 
